9107633bb5c156e30977a3d37a115c825aaeeb45
Jcai12321 <603480498@qq.com>
2020-09-10 19:42:05 +0800

    1

--- mysql/sql.sh
+++ mysql/sql.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 ############################################## mysql 相关笔记   ####################################
+# MySQL select from join on where group by having order by limit 执行顺序
+# 书写顺序：select [查询列表] from [表] [连接类型] join [表2] on [连接条件] where [筛选条件] group by [分组列表] having [分组后的筛选条件] order by [排序列表] limit [偏移, 条目数]
+# 执行顺序：from [表] [连接类型] join [表2] on [连接条件] where [筛选条件] group by [分组列表] having [分组后的筛选条件] order by [排序列表] limit [偏移, 条目数] select [查询列表]
+
+
 # 项目要求做一个访问统计的图表，要求统计粒度有日，周，月三个挡位
 # 按日统计
 SELECT
