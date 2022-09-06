8281620cd1ae9f6fb6f95b18e92f5d08fb078dd6
JaydenMan <603480498@qq.com>
2022-09-06 11:08:58 +0800

    写 crontab 任务时 %加转义

--- note/bash/linux.sh
+++ note/bash/linux.sh
@@ -2,5 +2,5 @@
 
 #获取昨天日期
 dt=`date --date='yesterday'  "+%Y-%m-%d"`
-
+# 写 crontab 任务时 %加转义 date --date='yesterday' "+\%Y-\%m-\%d"
 */3 0-8 * * * yesterday=`date --date='yesterday' "+%Y-%m-%d"`; curl -s "http://a.com/console/FbaManageTask/generateTransferFbaSuggest?date=${yesterday}" >> /home/wwwlogs/generateTransferFbaSuggest.log
