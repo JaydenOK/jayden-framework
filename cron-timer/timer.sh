b71328681dded3fac4def8857ed04a085325a605
JaydenOK <603480498@qq.com>
2022-10-20 16:04:51 +0800

    秒级定时器 ，间隔duration秒取命令文件smc_cmd.sh的命令执行

---
+++ cron-timer/timer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# 秒级定时器 ，间隔duration秒取命令文件smc_cmd.sh的命令执行
+#
+# 间隔 3s 取命令执行，此脚本每分钟执行:
+# * * * * * root /mnt/yibai_ac_system/appdal/smc/timer.sh > /dev/null 2>&1
+
+# 命令执行间隔duration
+duration=3
+
+minute=`date '+%M'`
+
+ScriptDir=$(cd `dirname $0`; pwd)
+
+lockFile=${ScriptDir}/smc.lock
+logFile=${ScriptDir}/smc.log
+cmdFile=${ScriptDir}/smc_cmd.sh
+
+function task() {
+    isLock=`cat ${lockFile}`
+    if [[ $isLock != "" ]]; then
+        echo "lock"
+        return
+    fi
+
+    echo `date '+%Y-%m-%d-%H-%M-%-S'` > $lockFile
+
+    cmd=`cat ${cmdFile}`
+
+    if [[ $cmd == "" ]]; then
+        echo -e "no cmd"
+        echo "" > $lockFile
+        return
+    fi
+
+    echo $cmd | tee -a $logFile &
+    $cmd | tee -a $logFile &
+
+    echo "" > $cmdFile
+    echo "" > $lockFile
+}
+
+for (( i = 0; i < 20; i++ )); do
+    if [[ $minute != `date '+%M'` ]]; then
+        echo "task exit"
+        break
+    fi
+    task
+    sleep $duration
+done
