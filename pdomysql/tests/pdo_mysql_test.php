da1f3f894bf87e60a4351c47038b0a851c9af879
JaydenOK <603480498@qq.com>
2022-11-15 14:56:17 +0800

    增加FluentPDO操作mysql数据库工具

---
+++ pdomysql/tests/pdo_mysql_test.php
@@ -0,0 +1,41 @@
+<?php
+
+//http://jayden.cc/pdomysql/tests/pdo_mysql_test.php
+
+require '../bootstrap.php';
+
+$config = [
+    'host' => '192.168.92.209',
+    'user' => 'xxxxxx',
+    'password' => 'xxxxxxxxxxx',
+    'dbname' => 'yibai_account_system',
+    'port' => '3306',
+    'charset' => 'utf8',
+];
+
+$pdo = new PDO("mysql:dbname={$config['dbname']};host={$config['host']};charset={$config['charset']}", "{$config['user']}", "{$config['password']}");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$pdo->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
+$fluent = new Envms\FluentPDO\Query($pdo);
+
+
+//查询
+$query = $fluent->from('yibai_async_config')->where('id', 9)->fetch();
+$query2 = $fluent->from('yibai_async_config')->where('exchange', 'async-message-exchange')->fetchAll();
+print_r($query);
+
+//新增
+//$values = array('title' => 'article 1', 'content' => 'content 1');
+//
+//$query = $fluent->insertInto('article')->values($values)->execute();
+//$query = $fluent->insertInto('article', $values)->execute(); // shorter version
+
+
+//更新
+$set = array('update_time' => date('Y-m-d H:i:s'));
+$query = $fluent->update('yibai_async_config')->set($set)->where('id', 9)->execute();
+
+//删除
+//$query = $fluent->deleteFrom('article')->where('id', 1)->execute();
+
+//Note: INSERT, UPDATE and DELETE queries will only run after you call ->execute()
