6ad37e9d339bddfbacb788bffad70463a7f825e3
JaydenOK <603480498@qq.com>
2022-12-15 21:26:42 +0800

    对象代理器

---
+++ utils/ObjectProxy.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * 对象代理器，属性，方法代理
+ * Class ObjectProxy
+ */
+
+class ObjectProxy
+{
+    /** @var object */
+    protected $__object;
+
+    public function __construct($object)
+    {
+        if (!is_object($object)) {
+            throw new \TypeError('Non-object given');
+        }
+        $this->__object = $object;
+    }
+
+    public function __getObject()
+    {
+        return $this->__object;
+    }
+
+    public function __get(string $name)
+    {
+        return $this->__object->{$name};
+    }
+
+    public function __set(string $name, $value): void
+    {
+        $this->__object->{$name} = $value;
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->__object->{$name});
+    }
+
+    public function __unset(string $name): void
+    {
+        unset($this->__object->{$name});
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        return $this->__object->{$name}(...$arguments);
+    }
+
+    public function __invoke(...$arguments)
+    {
+        /** @var mixed $object */
+        $object = $this->__object;
+        return $object(...$arguments);
+    }
+}
